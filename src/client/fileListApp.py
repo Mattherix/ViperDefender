eea21ec4ef373fb91d55081111f07ceb16c4c57e
Unaxe <74629449+Unaxe@users.noreply.github.com>
2023-06-12 12:29:25 +0200

    fix(client): fix the delete

--- src/client/fileListApp.py
+++ src/client/fileListApp.py
@@ -25,9 +25,6 @@ class FileListApp(tk.Tk):
         self.info_label = tk.Label(self, text="", font=("Helvetica", 12))
         self.info_label.pack(pady=10)
 
-        self.add_button = tk.Button(self, text="Add Item", command=self.add_item)
-        self.add_button.pack(pady=5)
-
         self.delete_button = tk.Button(self, text="Delete Item", command=self.delete_item)
         self.delete_button.pack(pady=5)
 
@@ -35,28 +32,29 @@ class FileListApp(tk.Tk):
 
     def add_item(self, text=None, additional_info=None):
         if text:
+            # add the item with his id and additional info
             self.text_list.append((text, additional_info))
+            self.update_treeview()
+        else:
+            # add the item with a default id and additional info
+            self.text_list.append(("New Item", "Additional Info"))
             self.update_treeview()
 
     def delete_item(self):
         if self.selected_item:
             confirmation = messagebox.askyesno("Delete Item", "Are you sure you want to delete this item?")
             if confirmation:
-                self.text_list.remove(self.selected_item)
+                del self.text_list[self.selected_item]
                 self.selected_item = None
                 self.update_treeview()
 
     def on_select(self, event):
         if self.treeview.selection():
-            item = self.treeview.selection()[0]
-            self.selected_item = self.treeview.item(item)["values"]
-            self.display_info()
+            print(self.treeview.selection())
+            self.selected_item = self.treeview.index(self.treeview.selection()[0])
+            print(self.selected_item)
 
-    def display_info(self):
-        if self.selected_item:
-            self.info_label.config(text=f"Selected Item: {self.selected_item[0]}\nAdditional Info: {self.selected_item[1]}")
-        else:
-            self.info_label.config(text="")
+
 
     def update_treeview(self):
         self.treeview.delete(*self.treeview.get_children())
@@ -64,7 +62,6 @@ class FileListApp(tk.Tk):
             self.treeview.insert("", tk.END, text=item[0], values=(item[1]))
 
         self.selected_item = None
-        self.display_info()
 
 if __name__ == "__main__":
     app = FileListApp()
