c3a44ec77c6a63ef7f3c819bc1a1a89b28279ad9
Unaxe <74629449+Unaxe@users.noreply.github.com>
2023-06-27 11:24:45 +0200

    fix(verification_script): add securities to avoid the missing script arg

--- src/verification/main.py
+++ src/verification/main.py
@@ -5,8 +5,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", help="The path of the executable to test")
 args = parser.parse_args()
-print(args)
-
+if args.path == None:
+    print("Please specify the path of the executable to test")
+    exit()
+if not os.path.exists(args.path):
+    print("The path of the executable to test is not valid")
+    exit()
 
 def check():
     # Get all the files in the documents folder
