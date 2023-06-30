1c1d286bee5172b0fc9b21ece338caa408bb0faf
Unaxe <74629449+Unaxe@users.noreply.github.com>
2023-06-30 11:19:01 +0200

    feat(verification_script): begin trafic verification

--- src/verification/main.py
+++ src/verification/main.py
@@ -2,6 +2,12 @@ import os
 from threading import Thread
 import time
 import argparse
+from scapy.all import sniff, TCP
+from scapy.layers.inet import IP
+
+
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", help="The path of the executable to test")
 args = parser.parse_args()
@@ -27,15 +33,20 @@ def check():
         }
         files_content.append(object)
 
+
+
+    # Create a thread to analyze the network traffic
+    analyze_thread = Thread(target=analyze_network_traffic, daemon=True)
+    analyze_thread.start()
+
     # Get the executable file that we want to run to test if it's a virus
-
     exectutable = os.path.abspath(args.path)
-
     # Run the executable
     daemon = Thread(target=os.system, args=(exectutable,), daemon=True)
     daemon.start()
     time.sleep(30)
 
+    analyze_thread.join()
 
     # Test if the files have changed
     for file in files_content:
@@ -53,6 +64,42 @@ def check():
                 print(f"File {file['name']} have been modified")
                 return False
     return True
+
+
+def analyze_network_traffic():
+    # Sniff network traffic for a certain duration
+    packets = sniff(timeout=30)
+
+    # Analyze the captured packets
+    for packet in packets:
+        if TCP in packet:
+            # Perform your analysis based on TCP packets
+            # You can access packet fields and perform various checks here
+            # Example: Check if the packet is a SYN flood attack
+            if packet[TCP].flags == 'S':
+                print("SYN flood attack detected!")
+
+        if IP in packet:
+            source_ip = packet[IP].src
+            destination_ip = packet[IP].dst
+
+            # Perform verifications based on source and destination IP addresses
+            # Example: Check if the packet is coming from or going to a suspicious IP
+            suspicious_ips = ["192.168.0.1", "10.0.0.2"]
+            if source_ip in suspicious_ips or destination_ip in suspicious_ips:
+                print(f"Suspicious network traffic detected with IP: {source_ip} -> {destination_ip}")
+
+            # Perform additional verifications or analysis as needed
+            # Example: Check for specific protocols or ports being used
+            if packet.haslayer(TCP):
+                source_port = packet[TCP].sport
+                destination_port = packet[TCP].dport
+
+                # Example: Check if traffic is being sent to a known malicious port
+                malicious_ports = [8080, 4444, 9999]
+                if destination_port in malicious_ports:
+                    print(
+                        f"Malicious port traffic detected: {source_ip}:{source_port} -> {destination_ip}:{destination_port}")
 
 if check():
     print("No virus detected")
